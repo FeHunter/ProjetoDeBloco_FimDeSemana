843e1cdc96d670d5ba9bdacb5978bc7134acc349
FeHunter <hunterfe1111@gmail.com>
2024-09-08 20:03:55 -0300

    Adicionando Contexto para Base relacional

---
+++ ProjetoDeBloco_FimDeSemana/Data/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoDeBloco_FimDeSemana.Models;
+
+namespace ProjetoDeBloco_FimDeSemana.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Usuario> Produtos { get; set; }
+    }
+}
--- ProjetoDeBloco_FimDeSemana/Program.cs
+++ ProjetoDeBloco_FimDeSemana/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoDeBloco_FimDeSemana.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// configurado o DbContexto
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
