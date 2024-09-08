6e3e9bfba31f3dc5a610dae1aa38d2545f2f7c89
FeHunter <hunterfe1111@gmail.com>
2024-09-08 19:53:22 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoDeBloco_FimDeSemana/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoDeBloco_FimDeSemana.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
