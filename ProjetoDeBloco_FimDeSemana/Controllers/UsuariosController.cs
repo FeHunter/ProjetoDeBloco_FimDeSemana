270ec00674a1a2223d2582942613e9b9d4583e89
FeHunter <hunterfe1111@gmail.com>
2024-09-15 19:29:02 -0300

    Adicioando criação de usuário

---
+++ ProjetoDeBloco_FimDeSemana/Controllers/UsuariosController.cs
@@ -0,0 +1,141 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjetoDeBloco_FimDeSemana.Data;
+using ProjetoDeBloco_FimDeSemana.Models;
+
+namespace ProjetoDeBloco_FimDeSemana.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly Contexto _context;
+
+        public UsuariosController(Contexto context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Usuarios.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NomeCompleto,NomeDeUsuario,Cpf,Email,Telefone")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NomeCompleto,NomeDeUsuario,Cpf,Email,Telefone")] Usuario usuario)
+        {
+            if (id != usuario.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(usuario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario != null)
+            {
+                _context.Usuarios.Remove(usuario);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return _context.Usuarios.Any(e => e.Id == id);
+        }
+    }
+}
--- ProjetoDeBloco_FimDeSemana/Models/Usuario.cs
+++ ProjetoDeBloco_FimDeSemana/Models/Usuario.cs
@@ -1,18 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjetoDeBloco_FimDeSemana.Models
 {
     public class Usuario
     {
+        [Key]
         public int Id { get; set; }
         public string NomeCompleto { get; set; }
         public string NomeDeUsuario { get; set; }
         public string Cpf { get; set; }
         public string Email { get; set; }
         public string Telefone { get; set; }
-        public DateTime DataDeNascimento { get; set; }
-        public string Genero { get; set; }
         public Pagamentos? Pagamento { get; set; }
         public List<Evento>? Eventos { get; set; }
-
-        public void PublicarAvaliacao() {}
     }
 }
