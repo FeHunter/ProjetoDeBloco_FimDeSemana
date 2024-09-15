b7a19430f558c419c87953b902c2eebeeb234ce3
FeHunter <hunterfe1111@gmail.com>
2024-09-15 20:04:31 -0300

    Cadastro do usuário.

--- ProjetoDeBloco_FimDeSemana/Models/Usuario.cs
+++ ProjetoDeBloco_FimDeSemana/Models/Usuario.cs
@@ -12,6 +12,7 @@ namespace ProjetoDeBloco_FimDeSemana.Models
         public string Email { get; set; }
         public string Telefone { get; set; }
         public Pagamentos? Pagamento { get; set; }
-        public List<Evento>? Eventos { get; set; }
+        public ICollection<Evento>? Eventos { get; set; }
+
     }
 }
