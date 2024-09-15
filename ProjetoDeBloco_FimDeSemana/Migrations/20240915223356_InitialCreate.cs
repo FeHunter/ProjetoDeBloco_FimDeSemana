b425917e4e228bcd7eca5cb4aa65b80dbb7e1376
FeHunter <hunterfe1111@gmail.com>
2024-09-15 19:34:24 -0300

    Criado migrations e banco de dados

---
+++ ProjetoDeBloco_FimDeSemana/Migrations/20240915223356_InitialCreate.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoDeBloco_FimDeSemana.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pagamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NomeCompleto = table.Column<string>(type: "TEXT", nullable: false),
+                    Cpf = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NomeCompleto = table.Column<string>(type: "TEXT", nullable: false),
+                    NomeDeUsuario = table.Column<string>(type: "TEXT", nullable: false),
+                    Cpf = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefone = table.Column<string>(type: "TEXT", nullable: false),
+                    PagamentoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Usuarios_Pagamentos_PagamentoId",
+                        column: x => x.PagamentoId,
+                        principalTable: "Pagamentos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UsuarioId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservas_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    Endereco = table.Column<string>(type: "TEXT", nullable: false),
+                    DataDoEvento = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ValorTotal = table.Column<double>(type: "REAL", nullable: false),
+                    HorarioDeFuncionamento = table.Column<string>(type: "TEXT", nullable: false),
+                    GerenciaReservaId = table.Column<int>(type: "INTEGER", nullable: true),
+                    UsuarioId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Eventos_Reservas_GerenciaReservaId",
+                        column: x => x.GerenciaReservaId,
+                        principalTable: "Reservas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Eventos_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cardapios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EventoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cardapios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cardapios_Eventos_EventoId",
+                        column: x => x.EventoId,
+                        principalTable: "Eventos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CardapiosPersonalizados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UsuarioId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CardapioDisponivelId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EventoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CardapiosPersonalizados", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CardapiosPersonalizados_Cardapios_CardapioDisponivelId",
+                        column: x => x.CardapioDisponivelId,
+                        principalTable: "Cardapios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CardapiosPersonalizados_Eventos_EventoId",
+                        column: x => x.EventoId,
+                        principalTable: "Eventos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItensDoCardapio",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CardapioId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Quantidade = table.Column<int>(type: "INTEGER", nullable: false),
+                    Preco = table.Column<double>(type: "REAL", nullable: false),
+                    ValorTotal = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItensDoCardapio", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItensDoCardapio_Cardapios_CardapioId",
+                        column: x => x.CardapioId,
+                        principalTable: "Cardapios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cardapios_EventoId",
+                table: "Cardapios",
+                column: "EventoId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CardapiosPersonalizados_CardapioDisponivelId",
+                table: "CardapiosPersonalizados",
+                column: "CardapioDisponivelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CardapiosPersonalizados_EventoId",
+                table: "CardapiosPersonalizados",
+                column: "EventoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Eventos_GerenciaReservaId",
+                table: "Eventos",
+                column: "GerenciaReservaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Eventos_UsuarioId",
+                table: "Eventos",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensDoCardapio_CardapioId",
+                table: "ItensDoCardapio",
+                column: "CardapioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservas_UsuarioId",
+                table: "Reservas",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_PagamentoId",
+                table: "Usuarios",
+                column: "PagamentoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CardapiosPersonalizados");
+
+            migrationBuilder.DropTable(
+                name: "ItensDoCardapio");
+
+            migrationBuilder.DropTable(
+                name: "Cardapios");
+
+            migrationBuilder.DropTable(
+                name: "Eventos");
+
+            migrationBuilder.DropTable(
+                name: "Reservas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Pagamentos");
+        }
+    }
+}
